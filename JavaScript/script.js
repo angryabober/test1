828c2ea889b99490e616336daad8ece995902463
eueueu12 <natali.voronina.06@mail.ru>
2023-08-17 20:48:26 +0300

    f

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,10 @@
-let a = prompt('число 1')
-let b = prompt('число 2')
+let isStudent = prompt('вы студент?')
+let hasJob= prompt('у вас есть работа?')
 
-if (a > b)
+if (isStudent == yes);{
+    alert('у студента есть работа');
+}
 
-alert(`${a} больше, чем ${b}`)
-
-if (a < b)
-
-alert(`${b} больше, чем ${a}`)
-
-if (a = b)
-
-alert(`${b} = ${a}`)
+if (hasJob == no);{
+    alert('у студента нет работы');
+}
