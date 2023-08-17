72ef803380f467abc9b8779f8ab30b0c051d8146
eueueu12 <natali.voronina.06@mail.ru>
2023-08-17 20:50:41 +0300

    f

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,9 @@
 let isStudent = prompt('вы студент?')
 let hasJob= prompt('у вас есть работа?')
 
-if (isStudent == yes);{
+if (isStudent == yes);
     alert('у студента есть работа');
-}
 
-if (hasJob == no);{
+
+if (hasJob == no);
     alert('у студента нет работы');
-}
