ee7f17e67594c75711f5b8de22e0d5fba2750219
eueueu12 <natali.voronina.06@mail.ru>
2023-08-29 19:28:44 +0300

    f f

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,7 +7,7 @@
 // }
 
 // let min = 0;
-// let max = 100;
+// let max = 999;
 // let count = 10;
 // while (count){
 //     let num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -17,7 +17,7 @@
 // console.log('всё')
 
 // let min = 0;
-// let max = 100;
+// let max = 999;
 // for (let count = 10; count;){
 //     let num = Math.floor(Math.random() * (max - min + 1)) + min;
 //     console.log(num);
