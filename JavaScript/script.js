e22af2a5606216d1cd63c9b37d3610546aaacdee
eueueu12 <natali.voronina.06@mail.ru>
2023-08-29 19:06:21 +0300

    f

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,26 @@
-let age = prompt('введите ваш возраст')
-if (age <= 18) {
-    alert(' школьник')
+// let age = prompt('введите ваш возраст')
+// if (age <= 18) {
+//     alert(' школьник')
+// }
+// else {
+//     alert('студент')
+// }
+
+// let min = 0;
+// let max = 100;
+// let count = 10;
+// while (count){
+//     let num = Math.floor(Math.random() * (max - min + 1)) + min;
+//     console.log(num);
+//     count = count - 1;
+// }
+// console.log('всё')
+
+let min = 0;
+let max = 100;
+for (let count = 10; count;){
+    let num = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(num);
+    count = count - 1;
 }
-else {
-    alert('студент')
-}
+console.log('всё')
