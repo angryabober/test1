f0dd207f66d8d91b6fe17e59c97c2b6befa08897
eueueu12 <natali.voronina.06@mail.ru>
2023-08-31 19:54:35 +0300

    f

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,23 @@
-let age = prompt('введите ваш возраст');
+// let age = prompt('введите ваш возраст');
 
-function agecalc(agex) {
-    if (agex < 18) {
-        alert('школьник')
-    }
-    else {
-        alert('студент')
-    }
+// function agecalc(agex) {
+//     if (agex < 18) {
+//         alert('школьник')
+//     }
+//     else {
+//         alert('студент')
+//     }
+// }
+
+// agecalc(age)
+
+let age = +prompt('age?')
+
+function myFunc(x){
+    if (x < 18)return 'школьник'
+    else return 'студент'
 }
 
-agecalc(age)
+let ageX = myFunc(age)
+
+alert(ageX)
